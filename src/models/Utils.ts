ae87ef9d18b6cc9f6959b92b09e4e31c629f3c09
Javier <java7f@gmail.com>
2021-11-19 13:29:53 -0400

    Changed to azure api

--- src/models/Utils.ts
+++ src/models/Utils.ts
@@ -2,7 +2,7 @@ export class Utils {
 
     //-------------------------------- API URLS --------------------------------//
 
-    public static BASE_API_URL = 'https://guidemeapi.azure-api.net'
+    public static BASE_API_URL = 'https://guideme.azurewebsites.net/api'
     //'http://localhost:5001/api' //Change this to use localhost or Azure hosted instance
     public static LOCATION_API_URL = `${Utils.BASE_API_URL}/Locations/location`
     public static AUDIOGUIDES_API_URL = `${Utils.BASE_API_URL}/Locations/audioguide`
